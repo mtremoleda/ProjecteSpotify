153504dd9c3a4bb3e1ed92cf1f27874d8636c2e0
mpicotti <mpicotti472@boscdelacoma.cat>
2025-10-14 16:57:40 +0200

    Eliminat OBJ i BIN

--- frontEnd/Admin/InterficieSpotify/InterficieSpotify/obj/Debug/net9.0-windows/InterficieSpotify.AssemblyInfo.cs
+++ frontEnd/Admin/InterficieSpotify/InterficieSpotify/obj/Debug/net9.0-windows/InterficieSpotify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterficieSpotify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef86fb78cb4189fc47b34084c763ec43edc66147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184811c8d2998dee83c467dc17dd61c8dd8453ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterficieSpotify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterficieSpotify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
