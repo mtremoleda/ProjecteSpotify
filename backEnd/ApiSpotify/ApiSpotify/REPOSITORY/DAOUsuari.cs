2ec4c493004186541d9bf253e9c060ba00c8ea68
EloiRigallPages <eloirigallp@gmail.com>
2025-10-14 18:33:38 +0200

    Crear el metode insert de DAOUsuari funcional

--- backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOUsuari.cs
+++ backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOUsuari.cs
@@ -1,5 +1,6 @@
 ﻿using ApiSpotify.MODELS; // Canvia-ho pel teu namespace real
 using ApiSpotify.Services;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -10,7 +11,21 @@ namespace ApiSpotify.REPOSITORY
     {
         public static void Insert(DatabaseConnection dbConn, Usuari usuari)
         {
+            dbConn.Open();
 
+            string sql = @"INSERT INTO Usuari (Id, Nom, Contrasenya, Salt)
+                           VALUES (@Id, @Nom, @Contrasenya, @Salt)";
+
+            using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+            cmd.Parameters.AddWithValue("@Id", usuari.Id);
+            cmd.Parameters.AddWithValue("@Nom", usuari.Nom);
+            cmd.Parameters.AddWithValue("@Contrasenya", usuari.Contrasenya);
+            cmd.Parameters.AddWithValue("@Salt", usuari.Salt);
+
+            int rows = cmd.ExecuteNonQuery();
+            Console.WriteLine($"{rows} fila inserida.");
+
+            dbConn.Close();
         }
     }
 }
