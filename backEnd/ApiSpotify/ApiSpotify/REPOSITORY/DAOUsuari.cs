dedb31fb07d4022b6d3986b97bb06f2b5a1692c9
EloiRigallPages <eloirigallp@gmail.com>
2025-10-14 18:25:53 +0200

    Crear el DAOUsuari

--- backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOUsuari.cs
+++ backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOUsuari.cs
@@ -1,6 +1,9 @@
-﻿namespace ApiSpotify.REPOSITORY
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using ApiSpotify.MODELS; // Canvia-ho pel teu namespace real
+
+namespace ApiSpotify.REPOSITORY
 {
-    public class DAOUsuari
-    {
-    }
+    
 }
