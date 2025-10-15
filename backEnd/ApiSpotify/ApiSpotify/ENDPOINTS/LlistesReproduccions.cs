a9ab2e123b1335c6601fbd026556378fd596e15a
marcel tremoleda <mtremoleda192@boscdelacoma.cat>
2025-10-15 20:48:55 +0200

    Get by id de playlist

--- backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/LlistesReproduccions.cs
+++ backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/LlistesReproduccions.cs
@@ -15,6 +15,14 @@ namespace ApiSpotify.ENDPOINTS
                 return Results.Ok(llistes);
             });
 
+            app.MapGet("/playlists/{id}", (Guid id) =>
+            {
+                LlistaReproduccio? llista = DAOLlistaReproduccio.GetById(dbConn, id);
+                return llista is not null
+                    ? Results.Ok(llista)
+                    : Results.NotFound(new { message = $"Playlist amb Id {id} no trobada." });
+            });
+
         }
     }
 }
