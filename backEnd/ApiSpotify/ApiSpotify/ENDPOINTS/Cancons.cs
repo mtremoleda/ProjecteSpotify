aeb6f8d38c4843a687f4a8c4798458e40cb6729c
marcel tremoleda <mtremoleda192@boscdelacoma.cat>
2025-10-15 17:40:38 +0200

    Get all i get by id de EndPoint Canço

--- backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/Cancons.cs
+++ backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/Cancons.cs
@@ -6,17 +6,30 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ApiSpotify.MODELS;
+using ApiSpotify.REPOSITORY;
 
 namespace ApiSpotify.ENDPOINTS
 {
-    public static class Cancons
+    public static class EndpointCancons
     {
         public static void MapProductEndpoints(this WebApplication app, DatabaseConnection dbConn)
         {
-            app.MapGet("/cançons", () =>
+            app.MapGet("/canco", () =>
             {
-                List<Canco> cancons = 
+                List<Canco> cancons = DAOCanco.GetAll(dbConn);
+                return Results.Ok(cancons);
             });
+
+            app.MapGet("/canco/{id}", (Guid id) =>
+            {
+                Canco? canco = DAOCanco.GetById(dbConn, id);
+
+                return product is not null
+                    ? Results.Ok(product)
+                    : Results.NotFound(new { message = $"Product with Id {id} not found." });
+            });
+
+
         }
     }
 }
