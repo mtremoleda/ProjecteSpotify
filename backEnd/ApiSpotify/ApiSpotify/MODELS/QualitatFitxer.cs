72575bbf7b84572af7fa9fb45f8bebe7b47eb9fd
EloiRigallPages <eloirigallp@gmail.com>
2025-10-15 18:02:19 +0200

    Arreglar els models

--- backEnd/ApiSpotify/ApiSpotify/MODELS/QualitatFitxer.cs
+++ backEnd/ApiSpotify/ApiSpotify/MODELS/QualitatFitxer.cs
@@ -4,7 +4,6 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid IdCanco { get; set; }
-        public Canco Canco { get; set; }
         public string Format { get; set; }
         public int? Bitrate { get; set; }
         public decimal? Mida { get; set; }
