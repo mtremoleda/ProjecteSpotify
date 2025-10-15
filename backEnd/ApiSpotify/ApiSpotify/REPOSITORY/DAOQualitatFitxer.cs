df3d7c7180dce37f460cb332d23dfb5ac7dbe5f1
EloiRigallPages <eloirigallp@gmail.com>
2025-10-15 17:57:53 +0200

    Cracio Delete DAOQUALITATFITXER

--- backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOQualitatFitxer.cs
+++ backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOQualitatFitxer.cs
@@ -104,5 +104,21 @@ namespace ApiSpotify.REPOSITORY
             dbConn.Close();
         }
 
+        public static bool Delete(DatabaseConnection dbConn, Guid id)
+        {
+            dbConn.Open();
+
+            string sql = @"DELETE FROM QualitatFitxer WHERE Id = @Id";
+
+            using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            int rows = cmd.ExecuteNonQuery();
+
+            dbConn.Close();
+
+            return rows > 0;
+        }
+
     }
 }
