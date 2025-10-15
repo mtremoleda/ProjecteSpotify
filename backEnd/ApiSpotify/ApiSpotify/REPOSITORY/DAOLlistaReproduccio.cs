cc6a37efde41b3333dd1c4f37aaa4c41d98b99cc
EloiRigallPages <eloirigallp@gmail.com>
2025-10-15 17:23:47 +0200

    Caviar la consulta sql per indicar-li amb el nom correcte la taula

--- backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOLlistaReproduccio.cs
+++ backEnd/ApiSpotify/ApiSpotify/REPOSITORY/DAOLlistaReproduccio.cs
@@ -55,7 +55,7 @@ namespace ApiSpotify.REPOSITORY
         {
             dbConn.Open();
 
-            string sql = "SELECT Id, IdUsuari, Nom FROM LlistaReproduccio WHERE Id = @Id";
+            string sql = "SELECT Id, IdUsuari, Nom FROM LlistaReproduccio WHERE Id = @Id FROM LlistaReproduccio";
 
             using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
             cmd.Parameters.AddWithValue("@Id", id);
