873e1679cd7359caf42929673b0864ea5eb8a5b5
marcel tremoleda <mtremoleda192@boscdelacoma.cat>
2025-10-15 18:09:23 +0200

    POST de Endpoint Canco

--- backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/Cancons.cs
+++ backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/Cancons.cs
@@ -14,22 +14,46 @@ namespace ApiSpotify.ENDPOINTS
     {
         public static void MapProductEndpoints(this WebApplication app, DatabaseConnection dbConn)
         {
-            app.MapGet("/canco", () =>
+            // GET ALL
+            app.MapGet("/cancons", () =>
             {
                 List<Canco> cancons = DAOCanco.GetAll(dbConn);
                 return Results.Ok(cancons);
             });
 
-            app.MapGet("/canco/{id}", (Guid id) =>
+            // GET BY ID
+            app.MapGet("/cancons/{id}", (Guid id) =>
             {
                 Canco? canco = DAOCanco.GetById(dbConn, id);
 
-                return product is not null
-                    ? Results.Ok(product)
-                    : Results.NotFound(new { message = $"Product with Id {id} not found." });
+                return canco is not null
+                    ? Results.Ok(canco)
+                    : Results.NotFound(new { message = $"Canco with Id {id} not found." });
             });
 
+            // POST /cancons
+            app.MapPost("/cancons", (CancoRequest req) =>
+            {
+                Canco canco = new Canco
+                {
+                    Id = Guid.NewGuid(),
+                    Titol = req.Titol,
+                    Artista = req.Artista,
+                    Album = req.Album,
+                    Durada = req.Durada
+                };
 
+                DAOCanco.Insert(dbConn, canco);
+
+                return Results.Created($"/cancons/{canco.Id}", canco);
+
+
+
+            });
         }
     }
+
 }
+
+    // DTO pel request
+    public record CancoRequest(String Titol, string Artista, string Album, decimal Durada);
