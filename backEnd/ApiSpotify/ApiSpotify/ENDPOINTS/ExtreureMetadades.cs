826a4c04f8fdb0309c2b57f634878fbc58b3ec9b
Eloi Rigall Pagès <erigall@eio.cat>
2025-10-16 11:58:08 +0200

    Declarar Endpoint ExtreureMetadades

---
+++ backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/ExtreureMetadades.cs
@@ -0,0 +1,13 @@
+﻿using ApiSpotify.Services;
+
+namespace ApiSpotify.ENDPOINTS
+{
+    public static class ExtreureMetadades
+    {
+        public static void MapExtreureMetadadesEndpoints(this WebApplication app, DatabaseConnection dbConn)
+        {
+
+
+        }
+    }
+}
