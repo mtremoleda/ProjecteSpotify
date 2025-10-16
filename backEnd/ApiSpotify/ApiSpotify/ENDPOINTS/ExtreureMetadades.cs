9b54f909b9bb173ab6f8db6f210bbe690cb109e6
Eloi Rigall Pagès <erigall@eio.cat>
2025-10-16 12:06:33 +0200

    ExtreureMetadades fer que es puguin pujar dos fitxers a la vegada

--- backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/ExtreureMetadades.cs
+++ backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/ExtreureMetadades.cs
@@ -1,4 +1,7 @@
-﻿using ApiSpotify.Services;
+﻿using ApiSpotify.MODELS;
+using ApiSpotify.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
 
 namespace ApiSpotify.ENDPOINTS
 {
@@ -6,8 +9,20 @@ namespace ApiSpotify.ENDPOINTS
     {
         public static void MapExtreureMetadadesEndpoints(this WebApplication app, DatabaseConnection dbConn)
         {
+            // POST /cancons/upload
+            app.MapPost("/cancons/upload", async ([FromForm] IFormFileCollection files) =>
+            {
+                if (files == null || files.Count == 0)
+                    return Results.BadRequest(new { message = "No s'ha rebut cap fitxer." });
 
+                // Col·lecció segura per a fils per guardar resultats
+                ConcurrentBag<Canco> canconsProcessades = new ConcurrentBag<Canco>();
 
+                // Processar amb un màxim de 2 fils
+                var options = new ParallelOptions { MaxDegreeOfParallelism = 2 };
+
+            });
         }
+    
     }
 }
