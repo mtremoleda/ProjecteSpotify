62f1d378903be6ba6acc1665fbb0a378d7ff7ecf
marcel tremoleda <mtremoleda192@boscdelacoma.cat>
2025-10-27 18:23:22 +0100

    creacio de DTO CancoRequest

--- backEnd/ApiSpotify/ApiSpotify/DTO/CanconsRequest.cs
+++ backEnd/ApiSpotify/ApiSpotify/DTO/CanconsRequest.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace ApiSpotify.DTO
-{
-    internal class CanconsRequest
+    //Post
+using ApiSpotify.MODELS;
+
+
+namespace ApiSpotify.DTO;
+
+    public record ProductRequest(string Titol, string Artista, string Album, Decimal Durada)
     {
+        // Guanyem CONTROL sobre com es fa la conversió
+
+        public Canco ToProduct(Guid id)   // Conversió a model
+        {
+            return new Canco
+            {
+                Id = id,
+                Titol = Titol,
+                Artista = Artista,
+                Album = Album,
+                Durada = Durada
+            };
+        }
     }
-}
+
+
