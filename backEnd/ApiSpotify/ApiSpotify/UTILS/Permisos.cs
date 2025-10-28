98e652cfc3e2773a4772430355a5ec1a6280590f
marcel tremoleda <mtremoleda192@boscdelacoma.cat>
2025-10-28 17:12:25 +0100

    creacio de Permisos + modificacio de model usuari

---
+++ backEnd/ApiSpotify/ApiSpotify/UTILS/Permisos.cs
@@ -0,0 +1,18 @@
+using System.Runtime.InteropServices;
+
+// En proyectos de estilo SDK como este, varios atributos de ensamblado que definían
+// en este archivo se agregan ahora automáticamente durante la compilación y se rellenan
+// con valores definidos en las propiedades del proyecto. Para obtener detalles acerca
+// de los atributos que se incluyen y cómo personalizar este proceso, consulte https://aka.ms/assembly-info-properties
+
+
+// Al establecer ComVisible en false, se consigue que los tipos de este ensamblado
+// no sean visibles para los componentes COM. Si tiene que acceder a un tipo en este
+// ensamblado desde COM, establezca el atributo ComVisible en true en ese tipo.
+
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el identificador de typelib, si este proyecto se expone
+// en COM.
+
+[assembly: Guid("0f60ce9d-0ab8-4605-a06f-10ee790ea6e4")]
