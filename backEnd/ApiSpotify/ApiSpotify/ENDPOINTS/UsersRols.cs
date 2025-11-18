853f5da7f129afc0fe1ac816f8bbdc3822d195bd
mpicotti <mpicotti472@boscdelacoma.cat>
2025-11-18 20:00:44 +0100

    EndPoint de UsersRols

---
+++ backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/UsersRols.cs
@@ -0,0 +1,54 @@
+﻿using ApiSpotify.REPOSITORY;
+using ApiSpotify.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSpotify.ENDPOINTS
+{
+    public static class EndointUsersRols
+    {
+
+        public static void MapUsersRolsEndpoints(this WebApplication app, DatabaseConnection dbConn)
+        {
+            // GET ALL
+            app.MapGet("/usersrols", () =>
+            {
+                List<UsersRols> usersrols = DAOUsersRols.GetAll(dbConn);
+                return Results.Ok(usersrols);
+            });
+
+
+
+            // POST /cancons
+            app.MapPost("/usersrols", ([FromBody] UsersRolsRequest req) =>
+            {
+                UsersRols usersrols = new UsersRols
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = req.UserId,
+                    RolId = req.RolId
+
+                };
+
+                DAOUsersRols.Insert(dbConn, usersrols);
+
+                return Results.Created($"/usersrols/{usersrols.Id}", usersrols);
+
+
+
+            });
+
+
+            app.MapDelete("/usersrols/{id}", (Guid id) =>
+                DAOUsersRols.Delete(dbConn, id) ? Results.NoContent() : Results.NotFound());
+
+        }
+    }
+}
+
+// DTO pel request
+public record UsersRolsRequest(Guid UserId, Guid RolId);
