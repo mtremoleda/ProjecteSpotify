c55fe846910382e25cc07f54473fa5a6eae6d681
EloiRigallPages <eloirigallp@gmail.com>
2025-11-19 18:03:05 +0100

    Fer el login api

---
+++ backEnd/ApiSpotify/ApiSpotify/ENDPOINTS/login.cs
@@ -0,0 +1,35 @@
+﻿using ApiSpotify.Services;
+using Microsoft.AspNetCore.Identity.Data;
+using ApiSpotify.REPOSITORY;
+using ApiSpotify.MODELS;
+
+namespace ApiSpotify.ENDPOINTS
+{
+    public static class login
+    {
+        public static void MaploginEndpoints(this WebApplication app, DatabaseConnection dbConn)
+        {
+            app.MapPost("/login", ([FromBody] LoginRequest req) =>
+            {
+                // 1. Obtener usuario por nombre
+                Usuari user = DAOUsuari.GetByNom(dbConn, req.Username);
+
+                if (user == null)
+                    return Results.Unauthorized();
+
+                // 2. Validar contraseña usando hash + salt
+                bool valid = PasswordHelper.VerifyPassword(
+                    req.Password,
+                    user.Contrasenya,
+                    user.Salt
+                );
+
+                if (!valid)
+                    return Results.Unauthorized();
+
+                return Results.Ok(new { message = "Login correcte", userId = user.Id });
+            });
+
+        }
+    }
+}
---
+++ backEnd/ApiSpotify/ApiSpotify/MODELS/login.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiSpotify.MODELS
+{
+    public class login
+    {
+        public string nom { get; set; }
+        public string contrasenya { get; set; }
+    }
+}
